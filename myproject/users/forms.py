b6b9b6d8541c1ab2d3605717b7f6d9269f58e5c3
nlinc1905 <nlinc1905@yahoo.com>
2019-09-20 01:46:45 -0400

    custom user added

---
+++ myproject/users/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta(UserCreationForm):
+        """
+        This class overrides the default fields 
+        for UserCreationForm
+        """
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age',)
+    
+
+class CustomUserChangeForm(UserChangeForm):
+    
+    class Meta:
+        """
+        This class overrides the default fields 
+        for UserChangeForm
+        """
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
