305a10a2f2224177e0caad671dd34d3d30e60b37
nlinc1905 <nlinc1905@yahoo.com>
2019-09-25 01:12:16 -0400

    user profile page added

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     HomePageView, 
-    AboutPageView
+    AboutPageView,
+    ProfilePageView,
 )
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('profile/', ProfilePageView.as_view(), name='profile'),
 ]
