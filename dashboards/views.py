63b818723aa2f3cc798535286169866f6e74f960
nlinc1905 <nlinc1905@yahoo.com>
2019-09-30 23:05:26 -0400

    login required for dashboards

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class HelloDashView(TemplateView):
+class HelloDashView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard_templates/hello_dash.html'
+    login_url = 'login'
